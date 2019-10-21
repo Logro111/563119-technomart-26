ddb0a6af9b454c4ab15cc399c627190d539e3c69
Logro111 <55096752+Logro111@users.noreply.github.com>
2019-10-21 04:09:08 +0300

    Добавил поддержку localStorage

--- js/modal_communication.js
+++ js/modal_communication.js
@@ -5,11 +5,27 @@ var name_field = modal_communication.querySelector("[name=name]");
 var email_field = modal_communication.querySelector("[name=email]");
 var letter_field = modal_communication.querySelector("[name=letter]");
 var form = modal_communication.querySelector("form");
+var storage_support = true;
+var storage_name
+var storage_email
+
+try {
+  storage_name = localStorage.getItem("name");
+  storage_email = localStorage.getItem("email");
+} catch (err) {
+  storage_support = false;
+}
 
 communication_link.addEventListener("click", function (evt){
   evt.preventDefault();
   modal_communication.classList.add("modal_shown");
-  name_field.focus();
+  if ( storage_name && storage_email ) {
+    name_field.value = storage_name;
+    email_field.value = storage_email;
+    letter_field.focus();
+  } else {
+    name_field.focus();
+  }
 });
 
 communication_close.addEventListener("click", function(evt) {
@@ -20,9 +36,14 @@ communication_close.addEventListener("click", function(evt) {
 
 form.addEventListener("submit", function(evt){
   if ( !name_field.value || !email_field.value || !letter_field.value ) {
-  evt.preventDefault();
-  modal_communication.classList.remove("modal_eror");
-  modal_communication.offsetWidth = modal_communication.offsetWidth;
-  modal_communication.classList.add("modal_eror");
-}
+    evt.preventDefault();
+    modal_communication.classList.remove("modal_eror");
+    modal_communication.offsetWidth = modal_communication.offsetWidth;
+    modal_communication.classList.add("modal_eror");
+  } else {
+    if (storage_support) {
+      localStorage.setItem("name", name_field.value);
+      localStorage.setItem("email", email_field.value);
+    }
+  }
 });
